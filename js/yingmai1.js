09e3bb11f765563d409a9b9f3a069e6cb302e79c
rmlcc <Rmlcc1993@outlook.com>
2018-12-28 11:56:05 +0800

    添加php部分测试源码

--- js/yingmai1.js
+++ js/yingmai1.js
@@ -299,12 +299,43 @@ $(document).on('pagecreate','#pagehome',function(){
 	window.location.href = 'https://www.kcash.com'
     });
 
+    var userName = document.getElementById('userName');
     var allMoneyValue = document.getElementById('allMoneyValue');
     var unlockValue = document.getElementById('unlockValue');
     var depositAddressText = document.getElementById('depositAddressText');
 
+    function setCookie(cname,cvalue,exdays){
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname+"="+cvalue+"; "+expires;
+    }
+    
+    function getCookie(cname){
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+            var c = ca[i].trim();
+            if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+	}
+	return "";
+    }
+    
+    function checkCookie(){
+	var user=getCookie("username");
+	if (user!=""){
+            alert("欢迎 " + user + " 再次访问");
+	}
+	else {
+            user = prompt("请输入你的名字:","");
+            if (user!="" && user!=null){
+		setCookie("username",user,30);
+            }
+	}
+    }
+    
     function setUser(user){
-	
+	userName.innerText = user;
     }
 
     function setShareValue(value){
@@ -329,10 +360,4 @@ $(document).on('pagecreate','#pagehome',function(){
     function setdePositAddressText(text){
 	depositAddressText.innerText = text;
     }
-
-    setShareValue(0);
-    setEarnMaxRate(0.99);
-    setAllMoneyValue(15);
-    setUnlockValue(15);
-    setdePositAddressText('1234');
 });
