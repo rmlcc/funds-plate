aa4ad7c70e2fb4b59f002840ec149ba5e571b90b
rmlcc <Rmlcc1993@outlook.com>
2019-01-04 08:55:18 +0800

    修复验证码bug

--- js/yingmai1.js
+++ js/yingmai1.js
@@ -408,7 +408,7 @@ $(document).on('pagecreate','#pagehome',function(){
 });
 
 $(document).on('pagecreate','#page2',function(){
-    $("sendCode1").on("tap",function(){
+    $("#sendCode1").on("tap",function(){
 	var mobile = $("#mobile2")[0].value;
 	$.post("/funds-plate/php/server.php",{type:'0',mobile:mobile},function(data,status){
 	    console.log("数据: \n" + data + "\n状态: " + status);
@@ -417,7 +417,7 @@ $(document).on('pagecreate','#page2',function(){
 });
 
 $(document).on('pagecreate','#page3',function(){
-    $("sendCode2").on("tap",function(){
+    $("#sendCode2").on("tap",function(){
 	var mobile = $("#mobile3")[0].value;
 	$.post("/funds-plate/php/server.php",{type:'0',mobile:mobile},function(data,status){
 	    console.log("数据: \n" + data + "\n状态: " + status);
