a455be7ad492eb691999bee8e0501699caf40f8d
rmlcc <Rmlcc1993@outlook.com>
2019-01-04 13:56:44 +0800

    添加数据库查询测试代码

--- php/test2.php
+++ php/test2.php
@@ -9,7 +9,18 @@
     die("Á¬½ÓÊ§°Ü: " . $conn->connect_error);
   }
 
-  $sql = "select * from user_table;";
-  $result = mysqli_query($conn, $sql);
-  echo mysqli_num_rows($result);
+  function check_user($username){
+    $sql = "select * from user_table;";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+      while($row = mysqli_fetch_assoc($result)) {
+        if ($row["user_name"] == $username) {
+	  return true;
+	}
+      }
+    }
+    return false;
+  }
+
+  echo check_user("13812341234");
 ?>
