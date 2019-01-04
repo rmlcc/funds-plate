6492c8700816b8bc04f5938108a16b45d9ad6298
rmlcc <Rmlcc1993@outlook.com>
2019-01-04 08:33:25 +0800

    修复语句错误

--- js/yingmai1.js
+++ js/yingmai1.js
@@ -345,7 +345,7 @@ $(document).on('pagecreate','#pagehome',function(){
 	$.post("/funds-plate/php/server.php",{type:'0',mobile:mobile},function(data,status){
 	    console.log("Êý¾Ý: \n" + data + "\n×´Ì¬: " + status);
 	});
-    }
+    });
 
     $("#buyToken").on("tap",function(){
 	window.location.href = 'https://www.7ebit.net'
